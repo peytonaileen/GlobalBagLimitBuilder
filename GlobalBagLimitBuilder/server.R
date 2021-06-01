79f8a810a0fcd7386c464506c34bdb5fc43767e0
peytonaileen <pamooreb@ucsb.edu>
2021-05-31 21:49:50 -0700

    Added in fishbase map to select species

--- GlobalBagLimitBuilder/server.R
+++ GlobalBagLimitBuilder/server.R
@@ -1,18 +1,187 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
-library(shinydashboard)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
     
+    #-----------------------------------------
+    # Read in data
+    #-----------------------------------------
+    ## Load the shape file to a Spatial Polygon Data Frame (SPDF) using the readOGR()     function
+    myspdf = readOGR(dsn=here("GlobalBagLimitBuilder", "data", "world_border"), layer="TM_WORLD_BORDERS_SIMPL-0.3")
+    
+    #Load fishbase country info
+    fish_master<-country()
+    
+    
+    #-----------------------------------------
+    # Create multiple pages 
+    #----------------------------------------- 
+    NUM_PAGES <- 5
+    rv <- reactiveValues(page = 1)
+    show("above1")
+    show("below1")
+    
+    observe({
+        toggleState(id = "prevBtn", condition = rv$page > 1)
+        toggleState(id = "nextBtn", condition = rv$page < NUM_PAGES)
+    })
+    
+    navPage <- function(direction) {
+        rv$page <- rv$page + direction
+    }
+    
+    observeEvent(input$prevBtn,{
+        lapply(1:NUM_PAGES, function(X) {hide(paste0("above", X)) })
+        lapply(1:NUM_PAGES, function(X) {hide(paste0("below", X)) })
+        navPage(-1)
+        show(paste0("above", rv$page))
+        if(rv$page == 2 | rv$page == 3) {
+            show(paste0("below3"))
+        } else {
+            show(paste0("below", rv$page))
+        }
+    } )
+    
+    observeEvent(input$nextBtn, {
+        lapply(1:NUM_PAGES, function(X) {hide(paste0("above", X)) })
+        lapply(1:NUM_PAGES, function(X) {hide(paste0("below", X)) })
+        navPage(1)
+        show(paste0("above", rv$page))
+        if(rv$page == 2 | rv$page == 3) {
+            show(paste0("below3"))
+        } else {
+            show(paste0("below", rv$page))
+        }
+    } )
 
-})
+    
+    #-----------------------------------------------
+    #  Create sidebar steps  
+    #----------------------------------------------- 
+    
+    #STEP ONE 
+    output$step_one_conditional<-reactive({
+        #!is.null(input$groupInput)
+    })
+    
+    output$selected_groupInput <- renderText({
+        #paste(input$groupInput, ";")
+    })
+    outputOptions(output, 'step_one_conditional', suspendWhenHidden = FALSE)
+    
+    #STEP TWO 
+    
+    output$step_two_conditional<-reactive({
+        #!is.null(input$selectedSpecies)
+    })
+    
+    output$selected_speciesInput <- renderText({
+        #paste(input$selectedSpecies, ";")
+    })
+    outputOptions(output, 'step_two_conditional', suspendWhenHidden = FALSE)
+    
+    #STEP Three
+    
+    output$step_three_conditional<-reactive({
+        #NROW(bag_proposals_data()) >0
+        
+    })
+    
+    output$current_proposals_text <- renderText({
+        #paste(bag_proposals_data()$Proposal,  ";")
+    })
+    outputOptions(output, 'step_three_conditional', suspendWhenHidden = FALSE)
+    
+    #--------------------------------------------
+    # Create species map to sort out a species list 
+    #--------------------------------------------
+    
+    #Mapping
+    output$mymap <- renderLeaflet({
+        # Create the map data and add polygons 
+        leaflet(data=myspdf) %>% 
+            addTiles() %>% 
+            setView(lat=10, lng=0, zoom=2.4) %>%
+            addPolygons(fillColor = "green",
+                        highlight = highlightOptions(weight = 5,
+                                                     color = "blue",
+                                                     fillOpacity = 0.7,
+                                                     bringToFront = TRUE),
+                        label = ~NAME,
+                        layerId = ~NAME) # add a layer ID to each shape. This will be used to identify the shape clicked
+        
+    })
+    
+    # Zoom and set the view after click on state shape
+    # input$mymap_shape_click will be NULL when not clicked initially to any shape
+    # input$mymap_shape_click will have the ID, lat and lng corresponding to the shape clicked
+    # Observe to update the view and zoom level when a shape is clicked
+    observe(
+        {  click = input$mymap_shape_click
+        #  subset the spdf object to get the lat, lng and country name of the selected shape (Country in this case)
+        sub = myspdf[myspdf$NAME==input$mymap_shape_click$id, c("LAT", "LON", "NAME")]
+        lat = sub$LAT
+        lng = sub$LON
+        country = sub$NAME
+        if(is.null(click)) ## if nothing is clicked
+            return()
+        else
+            leafletProxy("mymap") %>%
+            setView(lng = lng , lat = lat, zoom = 4) %>%
+            clearMarkers() %>%
+            addMarkers(lng =lng , lat = lat, popup = country)
+        # using lat long from spdf will not change the view on multiple clicks on the same shape
+        
+        }
+    )
+    
+    ## absolute Panel displaying country Name
+    output$map_text <- renderText({
+        sub = myspdf[myspdf$NAME==input$mymap_shape_click$id, c("LAT", "LON", "NAME")]
+        country <- sub$NAME
+        paste("Country:", country)
+    })
+    
+    map_species_list<-reactive({
+        sub = myspdf[myspdf$NAME==input$mymap_shape_click$id, c("LAT", "LON", "NAME")]
+        countryIn = sub$NAME
+        #Manual changes so that country names from map conform to fishbase country names
+        if(length(countryIn) > 0){
+            if(countryIn == "United States") countryIn<-"USA"
+            if(countryIn == "United Kingdom") countryIn <-"UK"
+            if(countryIn == "Marshall islands") countryIn <- "Marshall Is."
+            if(countryIn == "Northern Mariana islands") countryIn <- "North Marianas"
+        }
+        Y<-fish_master %>%
+            filter(
+                country %in% countryIn,
+                Status %in% c("endemic", "native", "introduced", "reintroduced")
+            )
+        Species<-sort(Y$Species)
+        FBname<-species(Species, fields=c("FBname", "PicPreferredName"))
+        data.frame(list(Species=Species, FBname=FBname$FBname, Pic=FBname$PicPreferredName))
+    })
+    
+    output$map_species_table <- renderDT({
+        datatable(map_species_list()[,1:2],
+                  colnames = c("Scientific name", "FishBase common name"),
+                  rownames = FALSE,
+                  selection = "single",
+                  options = list(pageLength = 15,
+                                 scrollX = TRUE,
+                                 columnDefs = list(list(className = 'dt-left', targets = 0:1)))
+        )
+        
+    })
+    map_species_table_Proxy<-dataTableProxy(session$ns('map_species_table'))
+    
+    observeEvent(input$map_species_table_rows_selected,{
+        shinyalert(
+            title = map_species_list()$FBname[input$map_species_table_rows_selected],
+            imageUrl = paste0("https://www.fishbase.de/images/species/", map_species_list()$Pic[input$map_species_table_rows_selected]),
+            imageWidth = 300,
+            imageHeight = 300,
+        )
+    })
+    
+}) # close server 
--- GlobalBagLimitBuilder/ui.R
+++ GlobalBagLimitBuilder/ui.R
@@ -1,18 +1,307 @@
 
+#----------------------------------
+# Creat global bag limit app  
+#----------------------------------
+
+#----------------------------------
+#Attach packages  
+#----------------------------------
+
+
+library(tidyverse)
+library(shinythemes)
+library(shinyWidgets)
+library(shinydashboard)
+library(here)
+library(ggplot2)
+library(ggridges)
+library(forcats)
+library(Hmisc)
+library(stringr)
+library(rsconnect)
+library(DT)
+library(treemap)
+library(shinyjs)
+library(kableExtra)
+library(ggplot2)
+library(ggradar2)
+library(gridExtra)
+library(plotly)
+library(aws.s3)
+library(shinyalert)
+library(shinybusy)
+library(htmltools)
+library(bsplus)
+library(shinyBS)
+library(rfishbase)
+library(leaflet)
+library(rgdal)
+
+#----------------------------------
+#Create the user interface:
+#----------------------------------
+
+tagList(
+    add_busy_spinner(
+        spin ="orbit",
+        color = "orange",
+        position = "top-right",
+        onstart = FALSE),
+    useShinyjs(),
+    useShinyalert(),
+    
+    tags$style(".popover{max-width:50%;}"),
 
 dashboardPage(
+    skin ="black", 
     dashboardHeader(title = "Global Bag Limit Builder"),
     dashboardSidebar(
         sidebarMenu(
             menuItem("About", tabName ="about"), 
+            menuItem("Create Species List", tabName = "createSpeciesList"),
             menuItem("Builder", tabName = "builder")
         )
     ),
     dashboardBody(
         tabItems(
             tabItem(tabName = "about", 
-                    fluidRow()), 
-            tabItem(tabName = "builder")
-        )
-    )
-)
+                    fluidRow(
+                        align = "center",
+                        h1("What is a 'Bag Limit Builder'?", class="text-success"),
+                        h3(
+                            "This tool is designed to allow users to simply and easily design and document draft bag limit proposals"
+                        ),
+                        br(),
+                        hr(), 
+                        br(),
+                        
+                        h2("Step 1&2: Select species to include", class="text-warning"),
+                        h4(
+                            "Develop your species list! Here you will have the opportunity to select the species group(s) you would like to include. Then you will have the option to further refine the list of species you wish to include. We recommend that individual species of particular concern in regard to either fishing or conservation objectives be evaluated separately."
+                        ),
+                        icon("fish", "fa-6x"), icon("fish", "fa-6x"),icon("fish", "fa-6x"),
+                        br(),
+                        br(),
+                        h2("Step 3: View historical catch data and develop bag limit proposals", class="text-warning"),
+                        h4(
+                            "There are fishing effort interview surveys from multiple sources across the state of Hawai'i built into the app to inform discussions. The data you will see reflects your species selections. Data is represented as take of number of fish per individual per day. You also have the option to filter by location and gear type. Addional filtering options will vary by source.  Mulitiple figures will apear reflecting your selection, in order to give an understanding of similarities and differences between the species and species groups you have selected. Ultimately, historical catch data are intended to provide a frame of reference for drafting bag limit proposals."), 
+                        br(), 
+                        icon("chart-area", "fa-8x"),
+                        br(), 
+                        h4("Now to draft a bag limit proposal. You will have an option to further subset species from your original selection, to reflect insights gained from the data and group discussion using the dropdown. Then you can describe proposed bag limits. This can be as specific as proposed limits for the group (i.e. Species X, Y, & Z at a bag limit of 10), or be more vague such as simply stating the group is of high cultural value to the community and should be treated with particular attention (i.e. Manini should be protected). Then you will be prompted to name the proposal. We recommend this name be easily identifiable and concise. Finally, when you are satisfied with your selection, press 'Save Proposal' in order to move the draft to your saved proposals. After the proposal has been saved, you are free to clear all current selections and select a new grouping."
+                        ),
+                        br(), 
+                        icon("edit", "fa-8x"),
+                        br(),
+                        br(),
+                        h2("Step 4: Evaluate your bag limit proposals", class="text-warning"),
+                        h4(
+                            "Here you will see all of your saved proposals that are ready to be evaluated. Through polling or group discussion, you can then adjust the decision criteria to reflect the strengths and weaknesses of the proposal. If it is decided that a proposal is not meeting objectives or needs to be re-evaluated completely, you can remove the proposal and it will be erased."
+                        ),
+                        br(),
+                        icon("balance-scale-left", "fa-8x"),
+                        h2("Step 5: Document your progress", class="text-warning"),
+                        h4(
+                            "Once you have drafted and evaluated as many proposals as you would like (or if you are finished with your session) you can save your progress by clicking the 'Download report' button. This will export a report summarizing all of the drafted proposals and their performance. "
+                        ),
+                        h4(
+                            "At any time you can also save your session progress on the 'Save & Restore' tab. Here you can save the current session to return to at a later date with the 'Save current session' and 'Load previous session' buttons."
+                        ),
+                        br(),
+                        icon("file-download", "fa-8x"),
+                        br()
+                    )), #Close about tab
+            tabItem(tabName = "createSpeciesList", 
+                    
+                    tabBox(title = "Create Species List",
+                           id = "listCreate",
+                           side = "right",
+                           width = "1000px",
+                           
+                           tabPanel("Species Map", 
+                                    useShinyalert(),
+                                    
+                                    leafletOutput("mymap"),
+                                    br(),
+                                    DTOutput("map_species_table"),
+                                    # Absolute panel will house the user input widgets
+                                    # Div tag is used to add styling to absolute panel
+                                    absolutePanel(top = 10, right = 10, fixed = FALSE,
+                                                  tags$div(style = "opacity: 0.70; background: #FFFFFF; padding: 8px; ",
+                                                           helpText("Welcome to the World map!"),
+                                                           textOutput("map_text") # display the country name in absolute panel when shape is clicked
+                                                  )
+                                    )), 
+                           tabPanel("Upload Creel Data", 
+                                    fileInput(
+                                        inputId = "customCreelData", 
+                                        label = "Choose a data file", 
+                                        accept = c(
+                                            'text/csv',
+                                            'text/comma-separated-values',
+                                            '.csv')),
+                                    switchInput(inputId ="header",
+                                                label = "Header",
+                                                onLabel = "Yes",
+                                                offLabel = "No", 
+                                                value = TRUE),
+                                    selectInput(inputId="exampData",
+                                                label="Example data",
+                                                choices=c("Creel - header",
+                                                          "Creel - no header")),
+                                    actionButton("exmpData",
+                                                 "Load Example Data",
+                                                 icon("cloud-upload"))))
+                    ),
+            tabItem(tabName = "builder", 
+                    sidebarLayout(
+                        sidebarPanel(
+                            style = "background: white; border: white;",
+                            width=3, 
+                            conditionalPanel(
+                                condition = "output.step_one_conditional", 
+                                align = "center",
+                                wellPanel(
+                                    id = "groupStep", 
+                                    style = "background-color: whitesmoke; border-color: #66CDAA; text-align:center;",
+                                    height="100%",
+                                    icon("check-circle", "fa-1x"), "Groups Selected:", 
+                                    tags$style(".fa-check-circle{color:orange;}"),
+                                    textOutput("selected_groupInput")
+                                ),
+                                icon("arrow-circle-down", "fa-3x"),
+                                br(), 
+                                br(),
+                            ),
+                            conditionalPanel(
+                                condition = "output.step_two_conditional", 
+                                align = "center",
+                                wellPanel(
+                                    id = "speciesStep", 
+                                    style = "background-color: whitesmoke; border-color: #66CDAA; text-align:center;",
+                                    height="100%",
+                                    icon("check-circle", "fa-1x"), "Species Selected:", 
+                                    textOutput("selected_speciesInput")
+                                ),
+                                icon("arrow-circle-down", "fa-3x"),
+                                br(), 
+                                br(),
+                                
+                            ), 
+                            conditionalPanel(
+                                condition = "output.step_three_conditional", 
+                                align = "center",
+                                wellPanel(
+                                    id = "proposalStep", 
+                                    style = "background-color: whitesmoke;
+              border-color:#66CDAA;
+              text-align:center;",
+              height="100%",
+              icon("check-circle",
+                   "fa-1x"),
+              "Proposal Drafts:",
+              textOutput("current_proposals_text")),
+              br(), 
+              br()), 
+              
+              
+              textAreaInput(inputId = "sessionNotes", 
+                            label = "Notes:", 
+                            placeholder = "Jot down any session notes here!",
+                            rows = 10),
+                        ),#Close sidebar panel
+              
+              
+              mainPanel(
+                  width=9,
+                  
+                  ########################################
+                  #Above previous / next
+                  ########################################
+                  
+                  hidden(
+                      wellPanel(
+                          id = "above1",
+                          style = "background-color: white;",
+                          "content here"
+                          
+                      )# close step one above 
+                      
+                  ),
+                  
+                  hidden(
+                      wellPanel(
+                          id = "above2",
+                          style = "background-color: white;",
+                          height="100%",
+                          h3("Step 2: Select species to discuss")
+                          
+                      )#close above step 2
+                      
+                  ),
+                  
+                  hidden(
+                      wellPanel(
+                          id = "above3",
+                          style = "background-color: white;",
+                          height="100%",
+                          h3("Step 3: View historical catch data and develop bag limit proposals")
+                      )# close above step 3 
+                  ),
+                  hidden(
+                      wellPanel(
+                          id = "above4",
+                          style = "background-color: white;",
+                          height="100%",
+                          h3("Step 4: Review and refine bag limit proposals")   
+                      )#close above step 4
+                  ),
+                  hidden(
+                      wellPanel(
+                          id = "above5",
+                          style = "background-color: white;",
+                          height="100%",
+                          h3("Step 5: Document your progress")
+                      )# close above step 5
+                  ),
+                  div(
+                      align="center",
+                      actionButton(
+                          inputId = "prevBtn",
+                          label="Previous Step",
+                          icon = icon('chevron-circle-left'), 
+                          style="color:white;background-color: teal;border-color: white"
+                      ),
+                      actionButton(inputId="nextBtn",
+                                   label = "Next Step", 
+                                   icon = icon('chevron-circle-right'), 
+                                   style="color:white;background-color: teal;border-color: white"), 
+                      
+                  ),
+                  ########################################
+                  #Below previous / next
+                  ########################################
+                  
+                  
+                  br(),
+                  hidden(
+                      wellPanel(
+                          id = "below3",
+                          style = "background-color: white;",
+                          h1("Below 3")
+                      )# close below step 3
+                  ),
+                  hidden(
+                      wellPanel(
+                          id = "below4",
+                          style = "background-color: white;",
+                          h3("Below 4")
+                      )# close below step 4
+                  )
+              )
+                    )# Close sidebar layout 
+              ) # Close tab item
+        )# Close tab items 
+    )# Close dashboard body 
+) # Close dashboard page 
+) # Close Tag 
